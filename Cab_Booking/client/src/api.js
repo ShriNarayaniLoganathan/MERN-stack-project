ed6d49ac88d069084c9203230509ab7a2949da5e
ShriNarayaniLoganathan <deeya1027@gmail.com>
2026-04-15 19:27:57 +0530

    updated API URL

--- Cab_Booking/client/src/api.js
+++ Cab_Booking/client/src/api.js
@@ -1,21 +1,24 @@
 import axios from "axios";
 
+// Use environment variable
+const BASE_URL = process.env.REACT_APP_API_URL;
+
 //🚕 Ride API
 const API = axios.create({
-  baseURL: "http://127.0.0.1:5000/api/rides",
+  baseURL: `${BASE_URL}/api/rides`,
 });
 
 //🔐 Auth API
 export const AuthAPI = axios.create({
-  baseURL: "http://127.0.0.1:5000/api/auth",
+  baseURL: `${BASE_URL}/api/auth`,
 });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
-  console.log("Sending token 👉",token)
-  if(token){
+  if (token) {
     req.headers.authorization = `Bearer ${token}`;
   }
   return req;
 });
+
 export default API;
