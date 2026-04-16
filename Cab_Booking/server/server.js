f9b8a2e8927c8d8a0a4f5d52e2573e533e01536a
ShriNarayaniLoganathan <deeya1027@gmail.com>
2026-04-16 11:45:33 +0530

    fixed server config

--- Cab_Booking/server/server.js
+++ Cab_Booking/server/server.js
@@ -21,6 +21,6 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(process.env.PORT, () => {
-  console.log("Server running on port", process.env.PORT);
+app.listen(PORT, () => {
+  console.log("Server running on port",PORT);
 });
